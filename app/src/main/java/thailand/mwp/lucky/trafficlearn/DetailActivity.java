85bb779f101534234296e7d91ba1be8033f2c172
androidlucky <luckymwp@gmail.com>
2016-09-12 14:50:31 +0700

    finish back_buttom in activity_detail

--- app/src/main/java/thailand/mwp/lucky/trafficlearn/DetailActivity.java
+++ app/src/main/java/thailand/mwp/lucky/trafficlearn/DetailActivity.java
@@ -1,7 +1,12 @@
 package thailand.mwp.lucky.trafficlearn;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,6 +15,7 @@ public class DetailActivity extends AppCompatActivity {
     //Explicit
     private TextView textView, detailTextView;
     private ImageView imageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +26,30 @@ public class DetailActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView4);
         detailTextView = (TextView) findViewById(R.id.textView5);
         imageView = (ImageView) findViewById(R.id.imageView2);
+        button = (Button) findViewById(R.id.button2);
 
         //Show View
         textView.setText(getIntent().getStringExtra("Title"));
         detailTextView.setText(getIntent().getStringExtra("Detail"));
         imageView.setImageResource(getIntent().getIntExtra("Image", R.drawable.traffic_01));
+
+        //Buttom Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.phonton1);
+                mediaPlayer.start();
+
+                //Show Web View
+                Intent intent = new Intent(DetailActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            } //onClick
+        });
+
+
 
     } //Main Method
 } //Main Class
